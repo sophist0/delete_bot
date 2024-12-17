6e622a1ffb1f3e6626afbdca2140c2fdb3b00915
Jonathan Stokes <stokesj84@gmail.com>
2024-12-17 14:57:52 -0800

    may have fixed browser load test

--- tests/test_base_util.py
+++ tests/test_base_util.py
@@ -8,13 +8,17 @@ import base_util as bu
 PARAMS_FILE = "config.txt"
 
 # Skip test on github for now because it would require running browsers in the github test env
-# chrome_path = Path('/usr/bin/google-chrome-stable')
-# @pytest.mark.skipif(not chrome_path.is_file(), reason="no chrome browser")
-# def test_load_browser_driver():
-#     params = bu.BotParams(PARAMS_FILE)
-#     driver_2 = bu.load_browser_driver(params)
-#     assert isinstance(driver_2, selenium.webdriver.chrome.webdriver.WebDriver)
-#     driver_2.quit()
+chrome_path = Path('/usr/bin/google-chrome-stable')
+@pytest.mark.skipif(not chrome_path.is_file(), reason="no chrome browser")
+def test_load_browser_driver():
+    params = bu.BotParams(PARAMS_FILE)
+
+    # On github the tests are run in a container without a display
+    params.CONTAINERIZE = True
+
+    driver_2 = bu.load_browser_driver(params)
+    assert isinstance(driver_2, selenium.webdriver.chrome.webdriver.WebDriver)
+    driver_2.quit()
 
 def test_BotParams():
     params = bu.BotParams(PARAMS_FILE)
